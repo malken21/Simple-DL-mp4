19d15ed0843bb02bab29053802aea54b8cc1ef47
Marumasa <77374813+malken21@users.noreply.github.com>
2025-01-11 17:48:52 +0900

    Update download.ps1

--- download.ps1
+++ download.ps1
@@ -1,5 +1,5 @@
 # コンソールで入力された動画のURLを代入
-$VideoURL = Read-Host -Prompt "動画のURLを入力してください"
+$VideoURL = Read-Host -Prompt "VideoURL"
 
 # ビデオ形式が MP4 でオーディオ形式が M4A の中で最高品質の動画をダウンロードする yt-dlp コマンドを実行
 Start-Process "yt-dlp -f `"bv*[ext=mp4]+ba[ext=m4a]`" $VideoURL"
