177e0a4f70f842a8611870705e55ee97509331c2
Marumasa <77374813+malken21@users.noreply.github.com>
2025-02-12 01:12:17 +0900

    Update download.ps1

--- download.ps1
+++ download.ps1
@@ -2,4 +2,4 @@
 $VideoURL = Read-Host -Prompt "VideoURL"
 
 # ビデオ形式が MP4 でオーディオ形式が M4A の中で最高品質の動画をダウンロードする yt-dlp コマンドを実行
-yt-dlp -f "bv*[ext=mp4]+ba[ext=m4a]" $VideoURL
+yt-dlp -f "bv*[ext=mp4]+ba[ext=m4a]" --extractor-arg "youtube:player_client=default,android_vr" $VideoURL
