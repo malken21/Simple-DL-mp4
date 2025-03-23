4c1620a604c318f547605c99926bacd43e6c2aff
Marumasa <77374813+malken21@users.noreply.github.com>
2025-03-24 01:21:40 +0900

    yt-dlp をアップデートするコマンドを追加

--- download.ps1
+++ download.ps1
@@ -1,5 +1,7 @@
 # コンソールで入力された動画のURLを代入
 $VideoURL = Read-Host -Prompt "VideoURL"
 
+# yt-dlp アップデート
+yt-dlp -U
 # ビデオ形式が MP4 でオーディオ形式が M4A の中で最高品質の動画をダウンロードする yt-dlp コマンドを実行
 yt-dlp -f "bv*[ext=mp4]+ba[ext=m4a]" --extractor-arg "youtube:player_client=default,android_vr" $VideoURL
